75252351e2be0b2f79d52003354fa4ab2c29d177
github username <github email address>
2019-02-08 16:54:00 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
-def nyc_pigeon_organizer(data)
+require 'pry'
+def nyc_pigeon_organizer(pigeon_data)
   # write your code here!
+pigeon_list = {}
+  pigeon_data.each do |traits, data|
+    data.each do |details, info|
+      info.each do |names|
+        pigeon_list[names] ||= {}
+        pigeon_list[names][traits] ||= []
+        pigeon_list[names][traits] << details.to_s
+    #  binding.pry
+    end
+  end
 end
+pigeon_list
+end
+
+# "Lucky" => {
+#     :color => ["purple"],
+#     :gender => ["male"],
+#     :lives => ["Central Park"]
+#   },
